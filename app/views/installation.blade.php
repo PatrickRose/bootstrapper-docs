b5bdd08b1d089b3519cf2ac25df57f7f08e0d265
Patrick Rose <pjr0911025+github@googlemail.com>
2014-01-03 09:44:18 +0000

    Finish installation docs. (patricktalmadge/bootstrapper#155)

--- app/views/installation.blade.php
+++ app/views/installation.blade.php
@@ -4,6 +4,20 @@
   Installation
 @stop
 
+@section('sidebar')
+  {{
+    Navigation::pills(
+      Navigation::links(
+        array(
+          array('Quick', '#quick'),
+          array('Detailed', '#detailed'),
+        )
+      ),
+      true
+    )
+    }}
+@stop
+
 @section('content')
   <div class='page-header'>
     <h1>
@@ -12,17 +26,6 @@
         Nice and easy!
       </small>
     </h1>
-  </div>
-
-  <div class='subnav'>
-    {{
-      Navigation::pills(
-        array(
-          array('label'=>'Quick', 'url'=>'#quick'),
-          array('label'=>'Detailed', 'url'=>'#detailed'),
-        )
-      )
-      }}
   </div>
 
   <section id='quick'>
@@ -34,55 +37,55 @@
     <p>
       Add the following to your <code>composer.json</code>:
     </p>
-<pre class="prettyprint linenums lang-javascript">
-"require": {
-  "patricktalmadge/bootstrapper": "dev-develop"
-}
-</pre>
+    <pre class="prettyprint linenums lang-javascript">
+      "require": {
+      "patricktalmadge/bootstrapper": "dev-develop"
+      }
+    </pre>
 
-  <p>
-    Register the Bootstrapper Service Provider:
-  </p>
+    <p>
+      Register the Bootstrapper Service Provider:
+    </p>
 
-<pre class="prettyprint linenums lang-php">
-'providers' => array(
-  'Bootstrapper\BootstrapperServiceProvider'
-)
-</pre>
+    <pre class="prettyprint linenums lang-php">
+      'providers' => array(
+      'Bootstrapper\BootstrapperServiceProvider'
+      )
+    </pre>
 
-  <p>
-    (Optional) Add the following aliases:
-  </p>
+    <p>
+      (Optional) Add the following aliases:
+    </p>
 
-<pre class="prettyprint linenums lang-php">
-'Alert' => 'Bootstrapper\\Alert',
-'Badge' => 'Bootstrapper\\Badge',
-'Breadcrumb' => 'Bootstrapper\\Breadcrumb',
-'Button' => 'Bootstrapper\\Button',
-'ButtonGroup' => 'Bootstrapper\\ButtonGroup',
-'ButtonToolbar' => 'Bootstrapper\\ButtonToolbar',
-'Carousel' => 'Bootstrapper\\Carousel',
-'DropdownButton' => 'Bootstrapper\\DropdownButton',
-'Form' => 'Bootstrapper\\Form',
-'Helpers' => 'Bootstrapper\\Helpers',
-'Icon' => 'Bootstrapper\\Icon',
-'Image' => 'Bootstrapper\\Image',
-'Label' => 'Bootstrapper\\Label',
-'MediaObject' => 'Bootstrapper\\MediaObject',
-'Navbar' => 'Bootstrapper\\Navbar',
-'Navigation' => 'Bootstrapper\\Navigation',
-'Paginator' => 'Bootstrapper\\Paginator',
-'Progress' => 'Bootstrapper\\Progress',
-'Tabbable' => 'Bootstrapper\\Tabbable',
-'Table' => 'Bootstrapper\\Table',
-'Thumbnail' => 'Bootstrapper\\Thumbnail',
-'Typeahead' => 'Bootstrapper\\Typeahead',
-'Typography' => 'Bootstrapper\\Typography',
-</pre>
+    <pre class="prettyprint linenums lang-php">
+      'Alert' => 'Bootstrapper\\Alert',
+      'Badge' => 'Bootstrapper\\Badge',
+      'Breadcrumb' => 'Bootstrapper\\Breadcrumb',
+      'Button' => 'Bootstrapper\\Button',
+      'ButtonGroup' => 'Bootstrapper\\ButtonGroup',
+      'ButtonToolbar' => 'Bootstrapper\\ButtonToolbar',
+      'Carousel' => 'Bootstrapper\\Carousel',
+      'DropdownButton' => 'Bootstrapper\\DropdownButton',
+      'Form' => 'Bootstrapper\\Form',
+      'Helpers' => 'Bootstrapper\\Helpers',
+      'Icon' => 'Bootstrapper\\Icon',
+      'Image' => 'Bootstrapper\\Image',
+      'Label' => 'Bootstrapper\\Label',
+      'MediaObject' => 'Bootstrapper\\MediaObject',
+      'Navbar' => 'Bootstrapper\\Navbar',
+      'Navigation' => 'Bootstrapper\\Navigation',
+      'Paginator' => 'Bootstrapper\\Paginator',
+      'Progress' => 'Bootstrapper\\Progress',
+      'Tabbable' => 'Bootstrapper\\Tabbable',
+      'Table' => 'Bootstrapper\\Table',
+      'Thumbnail' => 'Bootstrapper\\Thumbnail',
+      'Typeahead' => 'Bootstrapper\\Typeahead',
+      'Typography' => 'Bootstrapper\\Typography',
+    </pre>
 
-  <p>
-    And you're good to go!
-  </p>
+    <p>
+      Download and include the Twitter Bootstrap CSS and Javascript files and you're good to go!
+    </p>
 
   </section>
 
@@ -94,13 +97,13 @@
     </div>
 
     <p>
-      First, open up your <code>composer.json</code> file and add the following to your require list: 
+      First, open up your <code>composer.json</code> file and add the following to your require list:
     </p>
-<pre class="prettyprint linenums lang-javascript">
-"require": {
-  "patricktalmadge/bootstrapper": "dev-develop"
-}
-</pre>
+    <pre class="prettyprint linenums lang-javascript">
+      "require": {
+      "patricktalmadge/bootstrapper": "dev-develop"
+      }
+    </pre>
 
     <p>
       Then run <code>composer update</code> in your terminal and get a coffee/tea/your beverage of choice while it updates. I find that makes the wait much nicer.
@@ -110,52 +113,83 @@
       Then open up <code>app/config/app.php</code> and register the Bootstrapper Service Provider in the providers array:
     </p>
 
-<pre class="prettyprint linenums lang-php">
-'providers' => array(
-  'Bootstrapper\BootstrapperServiceProvider'
-)
-</pre>
+    <pre class="prettyprint linenums lang-php">
+      'providers' => array(
+      'Bootstrapper\BootstrapperServiceProvider'
+      )
+    </pre>
 
-  <p>
-    This means you'll get to use all the juicy goodness that Bootstrapper provides.
-  </p>
+    <p>
+      This means you'll get to use all the juicy goodness that Bootstrapper provides.
+    </p>
 
-  <p>
-    This bit's optional, but it does make life a lot easier in the long run so it's advised. <em>If you don't want to do this, that's fine - but since it saves you writing <code>Bootstrapper\Navbar::create()</code> (and lets you write <code>Navbar::create()</code>) with no performance hit...</em>. Add the following to your aliases array:
-  </p>
+    <p>
+      This bit's optional, but it does make life a lot easier in the long run so it's advised. <em>If you don't want to do this, that's fine - but since it saves you writing <code>Bootstrapper\Navbar::create()</code> (and lets you write <code>Navbar::create()</code>) with no performance hit...</em>. Add the following to your aliases array:
+    </p>
 
-<pre class="prettyprint linenums lang-php">
-'aliases' => array(
-  'Alert' => 'Bootstrapper\\Alert',
-  'Badge' => 'Bootstrapper\\Badge',
-  'Breadcrumb' => 'Bootstrapper\\Breadcrumb',
-  'Button' => 'Bootstrapper\\Button',
-  'ButtonGroup' => 'Bootstrapper\\ButtonGroup',
-  'ButtonToolbar' => 'Bootstrapper\\ButtonToolbar',
-  'Carousel' => 'Bootstrapper\\Carousel',
-  'DropdownButton' => 'Bootstrapper\\DropdownButton',
-  'Form' => 'Bootstrapper\\Form',
-  'Helpers' => 'Bootstrapper\\Helpers',
-  'Icon' => 'Bootstrapper\\Icon',
-  'Image' => 'Bootstrapper\\Image',
-  'Label' => 'Bootstrapper\\Label',
-  'MediaObject' => 'Bootstrapper\\MediaObject',
-  'Navbar' => 'Bootstrapper\\Navbar',
-  'Navigation' => 'Bootstrapper\\Navigation',
-  'Paginator' => 'Bootstrapper\\Paginator',
-  'Progress' => 'Bootstrapper\\Progress',
-  'Tabbable' => 'Bootstrapper\\Tabbable',
-  'Table' => 'Bootstrapper\\Table',
-  'Thumbnail' => 'Bootstrapper\\Thumbnail',
-  'Typeahead' => 'Bootstrapper\\Typeahead',
-  'Typography' => 'Bootstrapper\\Typography',
-)
-</pre>
+    <pre class="prettyprint linenums lang-php">
+      'aliases' => array(
+      'Alert' => 'Bootstrapper\\Alert',
+      'Badge' => 'Bootstrapper\\Badge',
+      'Breadcrumb' => 'Bootstrapper\\Breadcrumb',
+      'Button' => 'Bootstrapper\\Button',
+      'ButtonGroup' => 'Bootstrapper\\ButtonGroup',
+      'ButtonToolbar' => 'Bootstrapper\\ButtonToolbar',
+      'Carousel' => 'Bootstrapper\\Carousel',
+      'DropdownButton' => 'Bootstrapper\\DropdownButton',
+      'Form' => 'Bootstrapper\\Form',
+      'Helpers' => 'Bootstrapper\\Helpers',
+      'Icon' => 'Bootstrapper\\Icon',
+      'Image' => 'Bootstrapper\\Image',
+      'Label' => 'Bootstrapper\\Label',
+      'MediaObject' => 'Bootstrapper\\MediaObject',
+      'Navbar' => 'Bootstrapper\\Navbar',
+      'Navigation' => 'Bootstrapper\\Navigation',
+      'Paginator' => 'Bootstrapper\\Paginator',
+      'Progress' => 'Bootstrapper\\Progress',
+      'Tabbable' => 'Bootstrapper\\Tabbable',
+      'Table' => 'Bootstrapper\\Table',
+      'Thumbnail' => 'Bootstrapper\\Thumbnail',
+      'Typeahead' => 'Bootstrapper\\Typeahead',
+      'Typography' => 'Bootstrapper\\Typography',
+      )
+    </pre>
 
-  <p>
-    And you're good to go! Enjoy creating with Bootstrap!
-  </p>
-    
+    <p>
+      Now all you need to do is include the Bootstrap files. Feel free to do this however you want:
+    </p>
+
+    <ul>
+      <li>
+	Download <code>bootstrap.min.css</code> and <code>bootstrap.min.js</code> from <a href='http://getbootstrap.com'>Bootstrap</a>, and pop them in your public folder. Then do the same with <a href='http://jquery.com/download/'>Jquery</a>. Then use the following in your views to include Bootstrap and Jquery:
+	<ul>
+	  <li>
+	    <code class='prettyprint'>HTML::style('css/bootstrap.min.css')</code>
+	  </li>
+	  <li>
+	    <code class='prettyprint'>HTML::script('js/jquery.min.js')</code>
+	  </li>
+	  <li>
+	    <code class='prettyprint'>HTML::script('js/bootstrap.min.js')</code>
+	  </li>
+	</ul>
+      </li>
+      <li>
+	Use the various CDNs to include the relevant files:
+	<ul>
+	  <li>
+	    <code class='prettyprint'>HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css')</code>
+	  </li>
+	  <li>
+	    <code class='prettyprint'>HTML::script('http://code.jquery.com/jquery-1.10.1.min.js')</code>
+	  </li>
+	  <li>
+	    <code class='prettyprint'>HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js')</code>
+	  </li>
+	</ul>
+      </li>
+    </ul>
+
   </section>
 
 @stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,11 +1,19 @@
 <!doctype html>
 <html>
   <head>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css') }}
     <title>@yield('title') - Bootstrapper</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
     {{ HTML::style('app.css') }}
   </head>
-  <body data-spy="scroll" data-target=".subnav" data-offset="50">
+  <body data-spy="scroll" data-target=".bs-sidebar">
 
     <div class='container'>
       {{
@@ -16,12 +24,23 @@
             array(
               array('Home', url('')),
               array('Installation', url('installation')),
+	      array('Base CSS', url('css')),
             ))
         )
         ->collapsible()
         ->render()
         }}
-      @yield('content')
+      <div class='row'>
+        <div class='col-xs-3'>
+          <div class='bs-sidebar' data-spy="affix" data-affix-top="60">
+            @yield('sidebar')
+          </div>
+        </div>
+
+        <div class='col-xs-9'>
+          @yield('content')
+        </div>
+      </div>
 
       <footer class="footer">
         <p class="pull-right">
@@ -39,9 +58,9 @@
         <p>Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>. Documentation licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
       </footer>
     </div>
-    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    {{ HTML::script('https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js') }}
+    {{ HTML::script('http://code.jquery.com/jquery-1.10.1.min.js') }}
+    {{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js') }}
     {{ HTML::script('app.js') }}
   </body>
 </html>
